46dbb321cbd4fba99e8e21148adcee542c3f6e67
James Lett <jlett89@gmail.com>
2016-04-07 03:08:06 +0000

    Edit and Delete actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,51 +1,52 @@
 class ReviewsController < ApplicationController
-  before_action :find_book
-  before_action :find_review, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit]
+	before_action :find_play
+	before_action :find_review, only: [:edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :edit]
 
-  def new
-    @review = Review.new
-  end
-  
-  def create
-    @review = Review.new(review_params)
-    @review.play_id = @play_id
-    @review.user_id = current_user.id
-    
-    if @review.save
-      redirect_to play_path(@play)
-    else
-      render 'new'
-    end
-  end
+	def new
+		@review = Review.new
+	end
 
-  def edit
-  end
+	def create
+		@review = Review.new(review_params)
+		@review.play_id = @play.id
+		@review.user_id = current_user.id
 
-  def update
-    if @review.update(review_params)
-      redirect_to play_path(@play)
-    else
-      render 'edit'
-    end
-  end
+		if @review.save
+			redirect_to play_path(@play)
+		else
+			render 'new'
+		end
+	end
 
-  def destroy
-    @review.destroy
-    redirect_to book_path(@play)
-  end
+	def edit
+	end
 
-  private
+	def update	
+		if @review.update(review_params)
+			redirect_to play_path(@play)
+		else
+			render 'edit'
+		end
+	end
 
-  def review_params
-    params.require(:review).permit(:rating, :comment)
-  end
+	def destroy
+		@review.destroy
+		redirect_to play_path(@play)
+	end
 
-  def find_book
-    @play = Play.find(params[:play_id])
-  end
+	private 
 
-  def find_review
-    @review = Review.find(params[:id])
-  end
+		def review_params
+			params.require(:review).permit(:rating, :comment)
+		end
+
+		def find_play
+			@play = Play.find(params[:play_id])
+		end
+
+		def find_review
+			@review = Review.find(params[:id])
+		end
+
 end
